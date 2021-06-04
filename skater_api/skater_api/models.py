8bb8230f70b6f8f424cfe16b730941c4123b2e17
Ronnie Ross <62971289+ronnieross711@users.noreply.github.com>
2021-06-04 14:08:37 -0500

    models created

--- skater_api/skater_api/models.py
+++ skater_api/skater_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Skater(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.CharField(max_length=50)
+    home_town = models.CharField(max_length=100)
+    board_sponsor = models.CharField(max_length=120)
+    shoe_sponsor = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Tricks(models.Model):
+    trick_name = models.CharField(max_length=100)
+    signature_trick = models.CharField(max_length=100)
+    favorite_trick = models.CharField(max_length=120)
+    trick_location = models.CharField(max_length=150)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.trick_name
+
